3306b0db299b76915fb2a1930ab488c30a8dd788
mia <mn22nw@student.lnu.se>
2014-03-12 22:40:03 +0100

    Vers 1.0

---
+++ labb2punkt2/labb2punkt2/Model/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using labb2punkt2.Model;
+using labb2punkt2.Model.DAL;
+
+namespace labb2punkt2.Model
+{
+    public class Service  // kommunicerar med dataatkomstlagret som man kan instansiera i presentationslagret
+    {
+        private ContactDAL _contactDAL;
+
+        private ContactDAL
+
+
+        public IEnumerable<Contact> GetCustomer()
+        { 
+            
+
+
+        }
+    }
+}
